7009251e40f25d08c05d000e70c85ab35792ca09
danikachrunik@gmail.com Chrunik <danikachrunik@gmail.com>
2018-02-15 21:41:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -59,7 +59,7 @@ class TicTacToe
     puts "Please enter a position from 1 to 9:"
     input = gets.strip
     
-    if Integer(input)
+    if input.is_a?
       position = input_to_index(input)
         
       if valid_move?(position)
